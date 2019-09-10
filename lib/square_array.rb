030970ad8043153ebb7a2af279fc7b209fbcb495
Buford Eeds <bufordeeds8@gmail.com>
2019-09-10 16:39:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   counter = 0
-while array[counter] do
-  puts [counter**2]
-    counter +=1
-  end
-end
+
+   while array[counter] do
+     puts array[counter]
+     counter += 1
+   end
+ end
